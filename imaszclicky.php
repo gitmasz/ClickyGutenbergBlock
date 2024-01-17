f6cac6f42fdb1cd0338dff6a0b3a9ce6dbce7f54
Marcin Szczepkowski <m.szczepkowski@hotmail.com>
2024-01-17 18:00:37 +0100

    Updated plugin structure and metadata

--- imaszclicky.php
+++ imaszclicky.php
@@ -1,14 +1,14 @@
 <?php
 /**
- * Plugin Name:       Clicky
- * Description:       Example block scaffolded with Create Block tool.
+ * Plugin Name:       Clicky Block
+ * Description:       Plugin with example Gutenberg block named Clicky scaffolded with Create Block tool.
  * Requires at least: 6.1
  * Requires PHP:      7.0
  * Version:           0.1.0
- * Author:            The WordPress Contributors
+ * Author:            Marcin Szczepkowski
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
- * Text Domain:       clicky
+ * Text Domain:       imaszclicky
  *
  * @package           create-block
  */
@@ -24,7 +24,7 @@ if ( ! defined( 'ABSPATH' ) ) {
  *
  * @see https://developer.wordpress.org/reference/functions/register_block_type/
  */
-function clicky_clicky_block_init() {
-	register_block_type( __DIR__ . '/build' );
+function imaszclicky_block_init() {
+	register_block_type( __DIR__ . '/build/blocks/clickyGroup' );
 }
-add_action( 'init', 'clicky_clicky_block_init' );
+add_action( 'init', 'imaszclicky_block_init' );
